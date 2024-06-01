b756fa74479bbddd2e1d8d7274a2c1efdfee0caf
aliae2425 <noam.carmi80@gmail.com>
2024-06-01 23:42:45 +0200

    module 8 endend

--- Module6_Console2/Program.cs
+++ Module6_Console2/Program.cs
@@ -1,9 +1,13 @@
-﻿string str = "test"; 
-
-switch (str) {
-    case "test":
-        Console.WriteLine("test");
-    default:
-        Console.WriteLine("default");
-        break;
+﻿for (int i = 0; i < 20; i++)
+{
+    Console.Write(i);
+    if (i%3 == 0)
+    {
+        Console.Write("Fizz");
+    }
+    if (i%5 == 0)
+    {
+        Console.Write("Buzz");
+    }
+    Console.WriteLine();
 }
