7ca6cf755da392ba81864d13ae47f688209861e9
aliae2425 <noam.carmi80@gmail.com>
2024-06-09 02:23:19 +0200

    frist function

---
+++ Module9_Function/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Generating random numbers:");
+Console.WriteLine("How many random numbers do u want (default 5):");
+string temp = Console.ReadLine();
+if (temp == "") 
+{
+    DisplayRandomNumbers();
+}
+else 
+{
+    DisplayRandomNumbers(Convert.ToInt32(temp));
+}
+
+
+void DisplayRandomNumbers(int count = 5) 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < count; i++) 
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
+
+    Console.WriteLine();
+}
