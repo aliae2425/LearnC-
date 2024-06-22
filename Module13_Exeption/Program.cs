a61dc88b85f621db951c4d316caa3e4e76dd0bc4
aliae2425 <noam.carmi80@gmail.com>
2024-06-22 20:46:22 +0200

    module 12 ended

--- Module13_Exeption/Program.cs
+++ Module13_Exeption/Program.cs
@@ -1,33 +1,38 @@
 ﻿try
 {
-    Process1();
+    int num1 = int.MaxValue;
+    int num2 = int.MaxValue;
+    int result = num1 + num2;
+    Console.WriteLine("Result: " + result);
+
+    string str = null;
+    int length = str.Length;
+    Console.WriteLine("String Length: " + length);
+
+    int[] numbers = new int[5];
+    numbers[5] = 10;
+    Console.WriteLine("Number at index 5: " + numbers[5]);
+
+    int num3 = 10;
+    int num4 = 0;
+    int result2 = num3 / num4;
+    Console.WriteLine("Result: " + result2);
 }
-catch
+catch (OverflowException ex)
 {
-    Console.WriteLine("An exception has occurred");
+    Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
 }
-
-Console.WriteLine("Exit program");
-
-static void Process1()
+catch (NullReferenceException ex)
 {
-    try
-    {
-        WriteMessage();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
-    }
+    Console.WriteLine("Error: The reference is null." + ex.Message);
 }
-
-static void WriteMessage()
+catch (IndexOutOfRangeException ex)
 {
-    double float1 = 3000.0;
-    double float2 = 0.0;
-    int number1 = 3000;
-    int number2 = 0;
-
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    Console.WriteLine("Error: Index out of range." + ex.Message);
 }
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+}
+
+Console.WriteLine("Exiting program.");
