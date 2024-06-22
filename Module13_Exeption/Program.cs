b48d42a28959519a1d49cd48b56dbb653b418d62
aliae2425 <noam.carmi80@gmail.com>
2024-06-22 16:40:52 +0200

    try catch

--- Module13_Exeption/Program.cs
+++ Module13_Exeption/Program.cs
@@ -1,13 +1,33 @@
-﻿double float1 = 3000.0;
-double float2 = 0.0;
-int number1 = 3000;
-int number2 = 0;
+﻿try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
 
-try{
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+    }
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
     Console.WriteLine(float1 / float2);
     Console.WriteLine(number1 / number2);
 }
-catch{
-    Console.WriteLine("attention, division by zero!");
-}
-Console.WriteLine("Exit program");
