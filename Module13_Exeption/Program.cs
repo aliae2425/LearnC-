c65c3c4d9d676f84ff2586d350a386a8566d515f
aliae2425 <noam.carmi80@gmail.com>
2024-06-22 14:22:14 +0200

    testing expection

--- Module13_Exeption/Program.cs
+++ Module13_Exeption/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+catch{
+    Console.WriteLine("attention, division by zero!");
+}
+Console.WriteLine("Exit program");
