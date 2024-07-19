f17f246090917cead316f0fe4aaa9c17856e14e6
aliae2425 <noam.carmi80@gmail.com>
2024-07-19 22:40:05 +0200

    Content pannel

---
+++ Module19_WPF/WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
