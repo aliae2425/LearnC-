12b604c54a45080b958a0acb73dd90446b988a1c
aliae2425 <noam.carmi80@gmail.com>
2024-07-20 11:40:07 +0200

    WPF base complete

--- Module19_WPF/WpfApp1/MVVM/ViewModel/MainViewModel.cs
+++ Module19_WPF/WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -10,9 +10,42 @@ namespace WpfApp1.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand FamillyViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public FamillyViewModel FamillyVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            FamillyVM = new FamillyViewModel();
+            CurrentView = HomeVM;
+
+
+            HomeViewCommand = new RelayCommand(o => 
+                {
+                    CurrentView = HomeVM;
+                });
             
+            FamillyViewCommand = new RelayCommand(o => 
+                {
+                    CurrentView = FamillyVM;
+                });
         }
     }
 }
