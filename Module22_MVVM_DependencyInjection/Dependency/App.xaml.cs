c77111d526a75b9818109a4529709510ec62da12
aliae2425 <noam.carmi80@gmail.com>
2024-07-30 21:11:34 +0200

    cervaux qui fume

---
+++ Module22_MVVM_DependencyInjection/Dependency/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dependency
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider _ServiceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<HomeViewModel>();
+            services.AddSingleton<SettingsViewModel>();
+
+            _ServiceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = _ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
